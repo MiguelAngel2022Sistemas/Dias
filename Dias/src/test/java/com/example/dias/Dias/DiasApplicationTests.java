22a7331986a2f7a75cd46fd648895d213f6472e3
Wilder Julio Espinoza Bravo <wilderespinoza@bcp.com.pe>
2023-06-16 10:18:22 -0500

    Creacion de mi proyecto

---
+++ Dias/src/test/java/com/example/dias/Dias/DiasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dias.Dias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
